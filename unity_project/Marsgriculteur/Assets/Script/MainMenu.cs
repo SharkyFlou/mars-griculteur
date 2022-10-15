6d592b464e9a4c7ecb8d3969b65886263fbd23c8
[Paolo] [Hoogland] <phoogland@etu.unistra.fr>
2022-10-15 17:53:09 +0200

    paolo: mainmenu, cameramovement, camerazoom

---
+++ unity_project/Marsgriculteur/Assets/Script/MainMenu.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    
+   public void PlayGame()
+    {
+        //permet de charger les scenes qui se trouvent dans buildSettings->Scenes
+        //on les trie par nom ou par poisiton sur cette liste
+        SceneManager.LoadScene("Map");
+    }
+
+   
+}
