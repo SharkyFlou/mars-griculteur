ee5b1bce71b5712cd33434eb7ba326e3ff50a89c
phoogland <paolo.hoogland@etu.unistra.fr>
2022-11-15 16:00:54 +0100

    paolo push debut inventory

--- unity_project/Marsgriculteur/Assets/Script/Game/NotifPanel.cs
+++ unity_project/Marsgriculteur/Assets/Script/Game/NotifPanel.cs
@@ -5,10 +5,14 @@ using UnityEngine;
 public class NotifPanel : MonoBehaviour
 {
     public GameObject PanelNotif;
+    public GameObject NotifInventory;
+
 
     void Start()
     {
         PanelNotif.SetActive(false);
+        NotifInventory.SetActive(false);
+
     }
 
     public void OpenPanel()
@@ -24,4 +28,17 @@ public class NotifPanel : MonoBehaviour
         }
         
     }
+
+        //l'idee est d'ouvrir avec une fonction le panelInventory, une le panelNews, etc etc
+        //si possible faire avec le nom bouton, comme ça on utilise une seule fonction
+        public void OpenInventory(){
+            if (NotifInventory.activeSelf == false)
+            {
+                NotifInventory.SetActive(true);
+            }
+            else
+            {
+                NotifInventory.SetActive(false);
+            }
+        }
 }
