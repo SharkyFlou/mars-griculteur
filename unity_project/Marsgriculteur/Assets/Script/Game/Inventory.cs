007ab6d0e5795b3c55e3c2fd2be5e7987b948d34
Léo Niederberger <leo.niederberger-vialle@etu.unistra.fr>
2022-11-16 15:10:33 +0100

    rectification des sprites ajos, azloc, egro

--- unity_project/Marsgriculteur/Assets/Script/Game/Inventory.cs
+++ unity_project/Marsgriculteur/Assets/Script/Game/Inventory.cs
@@ -6,17 +6,22 @@ namespace game
 {
     abstract public class Inventory : MonoBehaviour
     {
-        private int weightMax=1000;
-        private Dictionary<BasicItem, int> slots = new Dictionary<BasicItem, int>();
+        private const int MAX_WEIGHT = 1000;
 
-//voir comment the fuck on recoit l'info de chaque item dans le slot
-        public string getInfoSlot()
+        private int weight;
+        
+        //Garder <string, int>
+        //Ajouter méthode allSeedPlant qui donne accès au données des basicItem
+        private Dictionary<string, int> slots = new Dictionary<string, int>();
+
+        //voir comment the fuck on recoit l'info de chaque item dans le slot
+        public int getNumberSlot(string key)
         {
-            return "AAAAAAH";
+            return this.slots[key];
         }
 
         public int getWeightMax(){
-            return this.weightMax;
+            return MAX_WEIGHT;
         }
     }
 }
