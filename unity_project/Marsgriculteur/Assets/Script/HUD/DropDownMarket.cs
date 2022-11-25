1574dc2934584d695d2f42c8be2b7562e6cf9f2a
Charly <charly.flu.67@gmail.com>
2022-11-25 19:02:00 +0100

    Choix affichage different graph market

---
+++ unity_project/Marsgriculteur/Assets/Script/HUD/DropDownMarket.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using game;
+using TMPro;
+
+public class DropDownMarket : MonoBehaviour
+{
+    public TMP_Dropdown dropdown;
+
+    public Market market;
+
+    public graphMarket ggraphMarket;
+
+    private List<EnumTypePlant> plTypeList;
+    // Start is called before the first frame update
+    void Start()
+    {
+        plTypeList = CreateAllSeedPlant.dicoPlant.getAllPlantType();
+        List<Plant> plList = new List<Plant> ();
+        foreach(EnumTypePlant plType in plTypeList)
+        {
+            plList.Add(CreateAllSeedPlant.dicoPlant.createPlant(plType));
+        }
+
+
+        dropdown.ClearOptions();
+        
+
+        List<TMP_Dropdown.OptionData> plantItems = new List<TMP_Dropdown.OptionData>();
+
+        foreach(Plant pl in plList)
+        {
+            TMP_Dropdown.OptionData plantItem = new TMP_Dropdown.OptionData(pl.getName(), pl.getImageLink());
+            plantItems.Add(plantItem);
+        }
+
+        dropdown.AddOptions(plantItems);
+    }
+
+    public void updateGraph(int newIndex)
+    {
+        ggraphMarket.changePlant(plTypeList[newIndex]);
+    }
+}
--- unity_project/Marsgriculteur/Assets/Script/HUD/graphMarket.cs
+++ unity_project/Marsgriculteur/Assets/Script/HUD/graphMarket.cs
@@ -3,8 +3,10 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using TMPro;
 using Unity.Mathematics;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 using static Unity.VisualScripting.Metadata;
@@ -26,41 +28,48 @@ public class graphMarket : MonoBehaviour
 
     private float yMaximum; //maximum de la hauteur, pour équilibrer l'affichage
     private float xSize; //maximum de la longueur, pour équilibrer l'affichage
-    private float xMaximum; 
+    private float xMaximum;
     private float graphHeight; //hauteur de container, calculé en amont pour permettre certaines modifications
     private float yMin;
     private int numberOfDays;
     private List<string> monthList;
+    const float mutlInvGraph = 2f;
+    private EnumTypePlant plantAct = EnumTypePlant.ELB;
 
     private List<GameObject> allChildsToSuppr = new List<GameObject>();
 
     private void Start()
     {
-        //instantie les list, c'est temporaire
-        //List<int> graphList = new List<int>() { 0, 11, 9, 8, 2, 3, 7, 8, 15, 19, 17, 13, 12, 11, 9, 8, 2, 3, 7, 8, 15, 19, 17, 13 , 12, 11, 9, 8, 2, 3, 7, 8, 15, 19, 17, 13 , 12, 11, 9, 8, 2, 3, 7, 8, 15, 19, 17, 13 , 12, 11, 9, 8, 2, 3, 7, 8, 15, 19, 17, 13 };
         monthList = new List<string>() { "Janv", "Fev", "Mars", "Avril", "Mai", "Juin", "Juil", "Aout", "Sept", "Oct", "Nov", "Dec" };
-        graphHeight = graphContainer.sizeDelta.y*0.9f; //hauteur du graph
+        graphHeight = graphContainer.rect.height * 0.9f; //hauteur du graph
         yMin = 10f; //pour rendre le graph plus beau
-
-
-        //ShowGraph(graphList); //affiche le graph
     }
 
-    public void affiche(){
-        List<int> graphList = market.last60Days(EnumTypePlant.ELB);
-        titre.text = "Cours du " + EnumTypePlant.ELB;
+    public void affiche()
+    {
+        List<int> graphList = market.last60Days(plantAct);
+        titre.text = "Cours du " + plantAct;
 
         yMaximum = graphList.Max(); //Y maximum pour l'affichage
-        xSize = (float)(graphContainer.sizeDelta.x / (graphList.Count * 1.1)); //espace entre les points en X
+        xMaximum = graphContainer.rect.width;
+        xSize = (float)(xMaximum / (graphList.Count * 1.1)); //espace entre les points en X
 
         numberOfDays = market.getDays();
 
-        if (graphList == null){
+        if (graphList == null)
+        {
             Debug.Log("????");
             return;
         }
 
+
         ShowGraph(graphList); //affiche le graph
+    }
+
+    public void changePlant(EnumTypePlant pl)
+    {
+        plantAct = pl;
+        affiche();
     }
 
 
@@ -68,14 +77,15 @@ public class graphMarket : MonoBehaviour
     {
         GameObject gameobject = new GameObject("circle", typeof(Image));  //créer une image, qui sera le point
 
-        gameobject.transform.SetParent(graphContainer,false);
+        gameobject.transform.SetParent(graphContainer, false);
         gameobject.GetComponent<Image>().sprite = circleSprite; //choix de l'image du point, permet de la choisir
         //on récupère les données type, taille, position et tt du points pour les modifier
-        RectTransform rectTransform = gameobject.GetComponent<RectTransform>(); 
+        RectTransform rectTransform = gameobject.GetComponent<RectTransform>();
         rectTransform.anchoredPosition = anchoredPosition;
         rectTransform.sizeDelta = new Vector2(11, 11);
         rectTransform.anchorMin = new Vector2(0, 0);
         rectTransform.anchorMax = new Vector2(0, 0);
+        rectTransform.localScale = new Vector2(mutlInvGraph, 1);
         allChildsToSuppr.Add(gameobject);
         return gameobject;
     }
@@ -92,7 +102,8 @@ public class graphMarket : MonoBehaviour
             //creer le texte de la ligne
             TextMeshProUGUI labelY = Instantiate(labelTemplateY, graphContainer, false);
             labelY.gameObject.SetActive(true);
-            labelY.rectTransform.anchoredPosition = new Vector2(0f,yMin + heightInScale * graphHeight);
+            labelY.rectTransform.anchoredPosition = new Vector2(0f, yMin + heightInScale * graphHeight);
+            labelY.rectTransform.localScale = new Vector2(mutlInvGraph, 1);
             allChildsToSuppr.Add(labelY.gameObject);
             labelY.GetComponent<TextMeshProUGUI>().text = ((int)math.round(heightInScale * yMaximum)).ToString();
 
@@ -100,7 +111,7 @@ public class graphMarket : MonoBehaviour
             //creer la ligne
             RectTransform dashY = Instantiate(dashTemplateY, graphContainer, false);
             dashY.gameObject.SetActive(true);
-            dashY.sizeDelta = new Vector2(graphContainer.sizeDelta.x, dashY.sizeDelta.y); //change la taille selon la taille du graph container
+            dashY.sizeDelta = new Vector2(xMaximum, dashY.sizeDelta.y); //change la taille selon la taille du graph container
             dashY.anchoredPosition = new Vector2(dashY.sizeDelta.x / 2, yMin + (heightInScale * graphHeight));
             allChildsToSuppr.Add(dashY.gameObject);
         }
@@ -111,16 +122,17 @@ public class graphMarket : MonoBehaviour
         for (int i = 0; i < valueList.Count; i++) //boucle pour chaque valeur, créer un point, et les relies
         {
             float xPosition = xSize + i * xSize;
-            float yPosition = yMin + (valueList[i]/ yMaximum) * graphHeight;
+            float yPosition = yMin + (valueList[i] / yMaximum) * graphHeight;
             //on récupère les données de type taille, position et tt du points pour les modifier
-            
+
             //creation du la valeur associé au X en abssice
-            if ((numberOfDays +i) % 5 == 0)
+            if ((numberOfDays + i) % 5 == 0)
             {
                 ///valeur
                 TextMeshProUGUI labelX = Instantiate(labelTemplateX, graphContainer, false);
                 labelX.gameObject.SetActive(true);
                 labelX.rectTransform.anchoredPosition = new Vector2(xPosition, -20f);
+                labelX.rectTransform.localScale = new Vector2(mutlInvGraph, 1);
                 int monthToDisplay = ((numberOfDays + i) / 5) % 12; //calcul complique, mais donne le mois 
                 labelX.GetComponent<TextMeshProUGUI>().text = monthList[monthToDisplay];
                 allChildsToSuppr.Add(labelX.gameObject);
@@ -129,13 +141,13 @@ public class graphMarket : MonoBehaviour
                 //trait
                 RectTransform dashX = Instantiate(dashTemplateX, graphContainer, false);
                 dashX.gameObject.SetActive(true);
-                dashX.sizeDelta = new Vector2(graphContainer.sizeDelta.y, dashX.sizeDelta.y); //change la taille selon la taille du graph container
+                dashX.sizeDelta = new Vector2(graphHeight, dashX.sizeDelta.y*mutlInvGraph); //change la taille selon la taille du graph container
                 dashX.anchoredPosition = new Vector2(xPosition, (dashX.sizeDelta.x / 2));
                 allChildsToSuppr.Add(dashX.gameObject);
             }
 
             GameObject circleGameObject = CreateCircle(new Vector2(xPosition, yPosition));
-            if(lastCircleGameObject != null) //si pas le premier point
+            if (lastCircleGameObject != null) //si pas le premier point
             {
                 //créer une ligne entre les deux points
                 CreateDotConnection(circleGameObject.GetComponent<RectTransform>().anchoredPosition, lastCircleGameObject.GetComponent<RectTransform>().anchoredPosition);
@@ -152,16 +164,18 @@ public class graphMarket : MonoBehaviour
         gameObject.GetComponent<Image>().color = new Color(1, 1, 1, 0.5f);
         allChildsToSuppr.Add(gameObject);
 
-        RectTransform rectTransform = gameObject.GetComponent<RectTransform>(); 
+        RectTransform rectTransform = gameObject.GetComponent<RectTransform>();
         Vector2 dir = (dotPositionA - dotPositionB).normalized;
         float distance = Vector2.Distance(dotPositionA, dotPositionB);
         float degres = AngleBetweenVector2(dotPositionB, dotPositionA); //calcul des degré entre les deux points
 
-        rectTransform.anchorMin = new Vector2(0,0);
+        rectTransform.anchorMin = new Vector2(0, 0);
         rectTransform.anchorMax = new Vector2(0, 0);
         rectTransform.sizeDelta = new Vector2(distance, 3f);
-        rectTransform.anchoredPosition = dotPositionB + dir*distance * 0.5f;
-        rectTransform.localEulerAngles = new Vector3(0, 0,degres);
+        rectTransform.localScale = new Vector2(1, mutlInvGraph);
+        rectTransform.anchoredPosition = dotPositionB + dir * distance * 0.5f;
+        rectTransform.localEulerAngles = new Vector3(0, 0, degres);
+
     }
 
     private float AngleBetweenVector2(Vector2 vec1, Vector2 vec2) //renvoie l'angle entre les deux points donnés
