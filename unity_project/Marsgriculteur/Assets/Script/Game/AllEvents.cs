d8d02257a83698d1e20615ffcb8781373e56a685
Elise Marqué <elise.marque@etu.unistra.fr>
2022-12-12 21:36:25 +0100

    uml fait

--- unity_project/Marsgriculteur/Assets/Script/Game/AllEvents.cs
+++ unity_project/Marsgriculteur/Assets/Script/Game/AllEvents.cs
@@ -5,12 +5,28 @@ using System;
 
 namespace game
 {
+    /// <summary>
+    /// La classe AllEvents permet de lister tous les événements qui peuvent arriver pendant une partie.
+    /// Elle utilise son constructeur pour créer tous les événements, et elle contient 2 méthodes : <c>getRandomEvent</c> et <c>substractDico</c>.
+    /// 
+    /// </summary>
     public class AllEvents
     {
 
         //name in first, and YES it already exist in the info of eventInfo, but i dont car 
         public Dictionary<string, EventInfo> allEventDico = new Dictionary<string, EventInfo>();
 
+        /// <summary>
+        /// Le constructeur <c>AllEvents</c> permet de créer les événements. Il y a 2 types d'événements : les événements dit cools et les événements dit pas cool.
+        /// Les événments sont composés d'un code unique (leur nom), d'une description, d'une durée (combien de temps ils vont durer après leur apparition), de 2 multiplieurs (le premier pour rajouter
+        /// un simple multiplier au prix, par exemple *0.9 ou 1.1, multiplier fixe qui reste actif tant que l'évent l'est
+        /// et le deuxième est un multiplier qui atteint son pic de multiplication au milieu de sa durée, ex : il vaut 2, et l'évent dure 5, alors il oscilera un peu près comme ça : 1.33, 1.66, 2, 1.66, 1.33),
+        /// ensuite il y a 3 booléens (le premier pour savoir si l'événement atteint les plantes, le deuxième si il atteint les graines et le troisième si il atteint les outils.
+        /// Ensuite il y a la liste des plantes (ou des graines, ça dépend ce que ça atteint) et la liste des outils, pour les outils atteint.
+        /// Les derniers paramètres sont : la probabilité d'apparition de l'événement, un événements ne peut qu'arriver à partir du xième jour,
+        /// l'image de l'événement et le dernier paramètres correspond au temps à partir duquel l'événement peut réapparaître après que celui-ci est commencé (ex : un event à une durée de 5, un cooldown de 10,
+        /// et un unlockAfter de 0, il peut arriver dès le 1er jour, et 10 jour après qu'il soit arrivé il peut re arriver).
+        /// </summary>
         public AllEvents()
         {
             //********************************************************************
@@ -160,7 +176,13 @@ namespace game
         }
 
         //get a new event depending on the month, and the impossible event
-        public EventInfo getRandomEvent(int month, Dictionary<EventInfo, int> impossibleEvents)
+        /// <summary>
+        /// La méthode <c>getRandomEvent</c> permet de générer un nouvel événement en fonction du jour et de l'événement impossible.
+        /// </summary>
+        /// <param name="day"></param>
+        /// <param name="impossibleEvents"></param>
+        /// <returns>Elle renvoie un événements.</returns>
+        public EventInfo getRandomEvent(int day, Dictionary<EventInfo, int> impossibleEvents)
         {
 
             Dictionary<string, EventInfo> possibleEvents = substractDico(allEventDico, impossibleEvents);
@@ -175,7 +197,7 @@ namespace game
             int totalProbablity = 0;
             foreach(EventInfo currentEvent in possibleEvents.Values) //calculate the sum of all event probabilites
             {
-                if(currentEvent.unlockableAfter > month) //verify if the event is possible
+                if(currentEvent.unlockableAfter > day) //verify if the event is possible
                 {
                     totalProbablity += currentEvent.probability; //add the probabilty of the event
                 }
@@ -187,7 +209,7 @@ namespace game
 
             foreach (EventInfo currentEvent in possibleEvents.Values)
             {
-                if (currentEvent.unlockableAfter > month) //verify if the event is possible
+                if (currentEvent.unlockableAfter > day) //verify if the event is possible
                 {
                     if ((randProba -= currentEvent.probability) <= 0) //decrease until it hits 0
                     {
@@ -202,6 +224,12 @@ namespace game
 
 
         //is utilize to get the possible Events, from all the events and the impossible ones
+        /// <summary>
+        /// La méthode <c>substractDico</c> est utilisée pour avoir les événements possibles en fonction de tous les événements et ceux qui sont impossibles.
+        /// </summary>
+        /// <param name="dicoOrigin"></param>
+        /// <param name="dicoSubstract"></param>
+        /// <returns>Elle retourne un dictionnaire(clé : String, valeur : EventInfo) d'événements possibles</returns>
         private Dictionary<string, EventInfo> substractDico(Dictionary<string, EventInfo> dicoOrigin, Dictionary<EventInfo, int> dicoSubstract)
         {
             Dictionary<string, EventInfo> newDico = new Dictionary<string, EventInfo>();
--- unity_project/Marsgriculteur/Assets/Script/HUD/GerePlant.cs
+++ unity_project/Marsgriculteur/Assets/Script/HUD/GerePlant.cs
@@ -5,89 +5,92 @@ using UnityEngine.UI;
 using game;
 using TMPro;
 
-public class GerePlant : MonoBehaviour
+namespace game
 {
-    public Inventory inventoryFunctions;
-    public ActivePanel reafficheInv;
-
-    /*     [SerializeField]
-     */
-    public PlotEvents StockedPlot;
-
-
-    private BasicItem stockedItem;
-    private int stockedQtt;
-
-    //sealed == FINAL en java
-    private int MAX_STOCKED_QTT = 20;
-
-    //appelee par le click sur le slot qu'on veut planter (seulement de type seed)
-    public void sendInfoClick(BasicItem item, int qtt)
+    public class GerePlant : MonoBehaviour
     {
-        stockedItem = item;
-        stockedQtt = qtt;
+        public Inventory inventoryFunctions;
+        public ActivePanel reafficheInv;
 
-        Transform[] goInactive = this.GetComponentsInChildren<Transform>();
+        /*     [SerializeField]
+         */
+        public PlotEvents StockedPlot;
 
-        //traite tous les enfants de panelPlot, permet de planter
-        foreach (Transform go in goInactive)
+
+        private BasicItem stockedItem;
+        private int stockedQtt;
+
+        //sealed == FINAL en java
+        private int MAX_STOCKED_QTT = 20;
+
+        //appelee par le click sur le slot qu'on veut planter (seulement de type seed)
+        public void sendInfoClick(BasicItem item, int qtt)
         {
-            //Debug.Log(go.name);
-            if (go.name == "TextItemSelected")
-            {
-                go.GetComponent<TextMeshProUGUI>().text = "Vous avez choisi : " + item.getName().ToString();
+            stockedItem = item;
+            stockedQtt = qtt;
 
-            }
-            if (go.name == "ImageSeed")
-            {
-                //Debug.Log("ok, image attribue");
-                //go.gameObject.SetActive(true);
-                go.GetComponent<Image>().sprite = item.getSprite();
-            }
-            if (go.name == "ButtonOKPlant")
-            {
-                //Debug.Log("ok, button attribue");
-                //go.GetComponent<Button>().onClick.AddListener(delegate { Soustrait(stockedItem, stockedQtt); });
+            Transform[] goInactive = this.GetComponentsInChildren<Transform>();
 
-                //cela ajoute un evenement soustraits au click du button ButtonOKPlant
-                go.GetComponent<Button>().onClick.AddListener(Soustraits); //elimine le nb choisi de graines a planter 
-                go.GetComponent<Button>().onClick.AddListener(StockedPlot.planteGraine); //doit appeler la fonction plategraine de PlotEvents
-                //@@@@@@@@@@@@@@@@@@@@@@@ ici on peut ajouter directement le enumTypePlant pour planteGraine @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-                //NE PAS AJOUTER () A LA FONCTION, SINON ON ENVOIE LE RESULTAT
+            //traite tous les enfants de panelPlot, permet de planter
+            foreach (Transform go in goInactive)
+            {
+                //Debug.Log(go.name);
+                if (go.name == "TextItemSelected")
+                {
+                    go.GetComponent<TextMeshProUGUI>().text = "Vous avez choisi : " + item.getName().ToString();
+
+                }
+                if (go.name == "ImageSeed")
+                {
+                    //Debug.Log("ok, image attribue");
+                    //go.gameObject.SetActive(true);
+                    go.GetComponent<Image>().sprite = item.getSprite();
+                }
+                if (go.name == "ButtonOKPlant")
+                {
+                    //Debug.Log("ok, button attribue");
+                    //go.GetComponent<Button>().onClick.AddListener(delegate { Soustrait(stockedItem, stockedQtt); });
+
+                    //cela ajoute un evenement soustraits au click du button ButtonOKPlant
+                    go.GetComponent<Button>().onClick.AddListener(Soustraits); //elimine le nb choisi de graines a planter 
+                    go.GetComponent<Button>().onClick.AddListener(StockedPlot.planteGraine); //doit appeler la fonction plategraine de PlotEvents
+                                                                                             //@@@@@@@@@@@@@@@@@@@@@@@ ici on peut ajouter directement le enumTypePlant pour planteGraine @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+                                                                                             //NE PAS AJOUTER () A LA FONCTION, SINON ON ENVOIE LE RESULTAT
+                }
             }
         }
-    }
 
-    //public void Soustrait(BasicItem item, int qtt)
-    //{
-    //    inventoryFunctions.SubstractFromInventory(item, qtt/2);
-    //    inventoryFunctions.displayInventory();
-    //}
+        //public void Soustrait(BasicItem item, int qtt)
+        //{
+        //    inventoryFunctions.SubstractFromInventory(item, qtt/2);
+        //    inventoryFunctions.displayInventory();
+        //}
 
-    public void Soustraits()
-    {
-        //IMPERATIF d'utiliser CreateAllSeedPlant.mainInventory.getInventory(), sinon on travaille avec un inventory qui est VIDE COMPLETEMENT
-        //Debug.Log(CreateAllSeedPlant.mainInventory.isDicoVide());
-
-        //si le dico n'est pas vide
-        if (!CreateAllSeedPlant.mainInventory.isDicoVide())
+        public void Soustraits()
         {
-            //on soustrait au item du dico la qtt voulue
-            inventoryFunctions.SubstractFromInventory(getStockedItem(), getStockedQtt(), CreateAllSeedPlant.mainInventory.getInventory());
-            //on reaffiche les elements
-            reafficheInv.Affiche();
+            //IMPERATIF d'utiliser CreateAllSeedPlant.mainInventory.getInventory(), sinon on travaille avec un inventory qui est VIDE COMPLETEMENT
+            //Debug.Log(CreateAllSeedPlant.mainInventory.isDicoVide());
+
+            //si le dico n'est pas vide
+            if (!CreateAllSeedPlant.mainInventory.isDicoVide())
+            {
+                //on soustrait au item du dico la qtt voulue
+                inventoryFunctions.SubstractFromInventory(getStockedItem(), getStockedQtt(), CreateAllSeedPlant.mainInventory.getInventory());
+                //on reaffiche les elements
+                reafficheInv.Affiche();
+            }
         }
+
+        public BasicItem getStockedItem()
+        {
+            return this.stockedItem;
+        }
+
+        public int getStockedQtt()
+        {
+            return this.stockedQtt;
+        }
+
+
     }
-
-    public BasicItem getStockedItem()
-    {
-        return this.stockedItem;
-    }
-
-    public int getStockedQtt()
-    {
-        return this.stockedQtt;
-    }
-
-
 }
