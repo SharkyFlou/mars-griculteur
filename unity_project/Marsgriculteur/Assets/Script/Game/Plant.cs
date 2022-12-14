86a08cf1dcf2b3d6755bce70aed87fe33e36c9e1
Elise Marqué <elise.marque@etu.unistra.fr>
2022-12-14 15:05:27 +0100

    Commentaires jusqu'à la classe shop

--- unity_project/Marsgriculteur/Assets/Script/Game/Plant.cs
+++ unity_project/Marsgriculteur/Assets/Script/Game/Plant.cs
@@ -5,9 +5,24 @@ using UnityEngine;
 
 namespace game
 {
+    /// <summary>
+    /// La classe <c>Plant</c> hérite de BasicPlant. Elle permet de créer les plantes qui seront récoltés et qui vont être vendu.
+    /// Elle possède une liste de prix.
+    /// </summary>
     public class Plant : BasicPlant
     {
         public List<int> basicPlantPrice;
+
+        /// <summary>
+        /// Le constructeur <c>Plant</c> permet de créer une plante.
+        /// </summary>
+        /// <param name="paraTypePlant">le type de la plante</param>
+        /// <param name="paraId">l'id de la plante</param>
+        /// <param name="paraName">le nom de la plante</param>
+        /// <param name="paraDescription">la description de la plante</param>
+        /// <param name="paraImagelink">le lien de l'image de la plante</param>
+        /// <param name="paraBasicPlantPrice">le prix de la plante</param>
+        /// <param name="paraPlantWeight">le poids de la plante</param>
         public Plant(EnumTypePlant paraTypePlant, int paraId, string paraName, string paraDescription, Sprite paraImagelink, List<int> paraBasicPlantPrice, int paraPlantWeight)
         {
             this.typePlante = paraTypePlant;
@@ -19,6 +34,9 @@ namespace game
             this.weight = paraPlantWeight;
         }
 
+        /// <summary>
+        /// Ce constructeur crée une plante "Error" si jamais il y a un problème
+        /// </summary>
         public Plant()
         {
             this.typePlante = EnumTypePlant.ELB;
@@ -30,6 +48,11 @@ namespace game
             this.weight = -1;
         }
 
+        /// <summary>
+        /// La méthode <c>getPrice</c> permet d'obtenir le prix de la plante
+        /// </summary>
+        /// <param name="month">la valeur du mois</param>
+        /// <returns>Elle retourne le prix</returns>
         public int getPrice(int month)
         {
             return basicPlantPrice[month % 12];
--- unity_project/Marsgriculteur/Assets/Script/Game/PlantedPlant.cs
+++ unity_project/Marsgriculteur/Assets/Script/Game/PlantedPlant.cs
@@ -5,18 +5,37 @@ using UnityEngine;
 
 namespace game
 {
+    /// <summary>
+    /// La classe <c>PlantedPlant</c> hérite de BasicPlant. Elle permet de créer une plante qui sera mise dans un champs.
+    /// Elle possède 2 sprites, un pour quand elle n'a pas poussé et un quand elle a poussé. Elle a aussi un temps de pousse et un type.
+    /// </summary>
     public class PlantedPlant : BasicPlant
     {
         private List<Sprite> spriteLinks;
         private int growthTime;
         private EnumTypePlant typePlant;
 
+        /// <summary>
+        /// Le constructeur <c>PlantedPlant</c> permet de créer une plante qui va être planté (juste avec son temps de pousse et à son type)
+        /// </summary>
+        /// <param name="paraTypePlant">le type de la plante</param>
+        /// <param name="paraGrowthTime">le temps de pousse</param>
         public PlantedPlant(EnumTypePlant paraTypePlant, int paraGrowthTime)
         {
             this.typePlant = paraTypePlant;
             this.growthTime = paraGrowthTime;
         }
 
+        /// <summary>
+        /// Le constructeur <c>PlantedPlant</c> permet de créer une plante qui va être planté.
+        /// </summary>
+        /// <param name="paraTypePlant">le type de plante</param>
+        /// <param name="paraId">l'id de la plante</param>
+        /// <param name="paraName">le nom de la plante</param>
+        /// <param name="paraDescription">la description de la plante</param>
+        /// <param name="paraImagelink">le lien de l'image de la plante</param>
+        /// <param name="paraSpriteLinks">le liste des images de la plante</param>
+        /// <param name="paraGrowthTime">le temps de pousse de la plante</param>
         public PlantedPlant(EnumTypePlant paraTypePlant, int paraId, string paraName, string paraDescription, Sprite paraImagelink, List<Sprite> paraSpriteLinks, int paraGrowthTime)
         {
             this.typePlante = paraTypePlant;
@@ -28,6 +47,9 @@ namespace game
             this.growthTime = paraGrowthTime;
         }
 
+        /// <summary>
+        /// Le constructeur <c>PlantedPlant</c> permet de créer une plante "Error" si jamais il y a une erreur.
+        /// </summary>
         public PlantedPlant()
         {
             this.typePlante = EnumTypePlant.ELB;
@@ -40,11 +62,19 @@ namespace game
 
         }
 
+        /// <summary>
+        /// La méthode <c>getSpriteLinks</c> permet d'obtenir les 2 images de la plante
+        /// </summary>
+        /// <returns>Elle retourne une liste des images.</returns>
         public List<Sprite> getSpriteLinks()
         {
             return spriteLinks;
         }
 
+        /// <summary>
+        /// La méthode <c>getGrowthTime</c> permet d'obtenir le temps de pousse de la plante.
+        /// </summary>
+        /// <returns>Elle retourne son temps de pousse</returns>
         public int getGrowthTime()
         {
             return growthTime;
