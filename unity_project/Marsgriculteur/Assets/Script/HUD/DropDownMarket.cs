92c8e719def26e99cbe74be6fc392f96a42d683b
Charly <charly.flu.67@gmail.com>
2022-12-29 11:33:03 +0100

    Modification package game plos logique dans le PUML et le code

--- unity_project/Marsgriculteur/Assets/Script/HUD/DropDownMarket.cs
+++ unity_project/Marsgriculteur/Assets/Script/HUD/DropDownMarket.cs
@@ -1,58 +1,56 @@
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using game;
 
-namespace game
+/// <summary>
+/// La classe <c>DropDownMarket</c> permet d'afficher le graphe et les plantes.
+/// Elle possède 4 attributs : dropdown, market, graphMarket, plTypeList.
+/// </summary>
+public class DropDownMarket : MonoBehaviour
 {
+    public TMP_Dropdown dropdown;
+
+    public Market market;
+
+    public GraphMarket graphMarket;
+
+    private List<EnumTypePlant> plTypeList;
+
     /// <summary>
-    /// La classe <c>DropDownMarket</c> permet d'afficher le graphe et les plantes.
-    /// Elle possède 4 attributs : dropdown, market, graphMarket, plTypeList.
+    /// La méthode <c>Start</c> est utilisée pour le démarrage. Étant donné que Start n'est appelée qu'une seule fois, elle permet d'initialiser les éléments
+    /// qui doivent persister tout au long de la vie du script, mais ne doivent être configurés qu'immédiatement avant utilisation.
+    /// Pour notre cas elle récupère tout les type de plante, afin de pouvoir les afficher dans un dropdown
     /// </summary>
-    public class DropDownMarket : MonoBehaviour
+    void Start()
     {
-        public TMP_Dropdown dropdown;
-
-        public Market market;
-
-        public GraphMarket graphMarket;
-
-        private List<EnumTypePlant> plTypeList;
-
-        /// <summary>
-        /// La méthode <c>Start</c> est utilisée pour le démarrage. Étant donné que Start n'est appelée qu'une seule fois, elle permet d'initialiser les éléments
-        /// qui doivent persister tout au long de la vie du script, mais ne doivent être configurés qu'immédiatement avant utilisation.
-        /// Pour notre cas elle récupère tout les type de plante, afin de pouvoir les afficher dans un dropdown
-        /// </summary>
-        void Start()
+        plTypeList = CreateAllSeedPlant.dicoPlant.getAllPlantType();
+        List<Plant> plList = new List<Plant>();
+        foreach (EnumTypePlant plType in plTypeList)
         {
-            plTypeList = CreateAllSeedPlant.dicoPlant.getAllPlantType();
-            List<Plant> plList = new List<Plant>();
-            foreach (EnumTypePlant plType in plTypeList)
-            {
-                plList.Add(CreateAllSeedPlant.dicoPlant.createPlant(plType));
-            }
-
-
-            dropdown.ClearOptions();
-
-
-            List<TMP_Dropdown.OptionData> plantItems = new List<TMP_Dropdown.OptionData>();
-
-            foreach (Plant pl in plList)
-            {
-                TMP_Dropdown.OptionData plantItem = new TMP_Dropdown.OptionData("", pl.getSprite());
-                plantItems.Add(plantItem);
-            }
-            dropdown.AddOptions(plantItems);
+            plList.Add(CreateAllSeedPlant.dicoPlant.createPlant(plType));
         }
 
-        /// <summary>
-        /// La méthode <c>updateGraph</c> permet de mettre à jour le graph
-        /// </summary>
-        /// <param name="newIndex">le nouveau point du graph</param>
-        public void updateGraph(int newIndex)
+
+        dropdown.ClearOptions();
+
+
+        List<TMP_Dropdown.OptionData> plantItems = new List<TMP_Dropdown.OptionData>();
+
+        foreach (Plant pl in plList)
         {
-            graphMarket.changePlant(plTypeList[newIndex]);
+            TMP_Dropdown.OptionData plantItem = new TMP_Dropdown.OptionData("", pl.getSprite());
+            plantItems.Add(plantItem);
         }
+        dropdown.AddOptions(plantItems);
+    }
+
+    /// <summary>
+    /// La méthode <c>updateGraph</c> permet de mettre à jour le graph
+    /// </summary>
+    /// <param name="newIndex">le nouveau point du graph</param>
+    public void updateGraph(int newIndex)
+    {
+        graphMarket.changePlant(plTypeList[newIndex]);
     }
 }
